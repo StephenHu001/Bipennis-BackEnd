c509c6648a44d761cfe8336f06d50df98d6a62dc
Stephen Hu <stephenhu031028@gmail.com>
2025-05-20 23:26:33 +0800

    UserService Base

--- services/user-service/src/main/java/com/stephenhu/bipennis/userservice/mapper/AddressMapper.java
+++ services/user-service/src/main/java/com/stephenhu/bipennis/userservice/mapper/AddressMapper.java
@@ -2,12 +2,14 @@ package com.stephenhu.bipennis.userservice.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.stephenhu.bipennis.model.PO.userservice.AddressPO;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
 * @author Stephen Hu
 * @description 针对表【t_address(地址信息表)】的数据库操作Mapper
 * @createDate 2025-05-19 15:23:59
 */
+@Mapper
 public interface AddressMapper extends BaseMapper<AddressPO> {
 
 }
